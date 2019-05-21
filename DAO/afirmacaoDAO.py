f025c2bb3b12e3c1f700c249f2192deb87a30fbb
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2019-05-20 23:10:27 -0300

    Implementado a fala do edgar, e canto de música

--- DAO/afirmacaoDAO.py
+++ DAO/afirmacaoDAO.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+from os import system
+from helpers.validacaohelper import ValidacaoHelper
 
 
 class AfirmacaoDAO:
@@ -12,10 +14,6 @@ class AfirmacaoDAO:
             statement = statements.find_one({"text": afirmacao})
             self.id = statement['_id']
 
-    def alterar(self, nova_afirmacao):
-        statements = self._get_banco()
-        statements.update({"_id": self.id}, {"$set": {"text": nova_afirmacao}})
-
     def alterar_em_resposta_a(self, novo_em_resposta_a):
         statements = self._get_banco()
         statements.update({"_id": self.id}, {"$set": {"in_response_to": novo_em_resposta_a}})
@@ -28,8 +26,25 @@ class AfirmacaoDAO:
             statements.update({'_id': self.id}, {'$set': {nome: valor}})
 
     # Metodo a ser deletado após implementação da classe de conexão
+
+    def alterar(self, nova_afirmacao):
+        statements = self._get_collection("statements")
+        statements.update({"_id": self.id}, {"$set": {"text": nova_afirmacao}})
+
+    # Metodo a ser deletado após implementação da classe de conexão
     def _get_banco(self):
+        if not ValidacaoHelper.rodando('mongo'):
+            self._start_mongo_service()
         cliente = MongoClient('127.0.0.1', 27017)
         db = cliente['chatterbot-database']
-        statements = db.statements
-        return statements
+        return db
+
+    def _start_mongo_service(self):
+        command_start_mongo_service = "systemctl start mongodb"
+        senha_sudo = "senha123"
+        system(f'echo "{senha_sudo}" | sudo -S {command_start_mongo_service}')
+
+    def _get_collection(self, collection):
+        db = self._get_banco()
+        collection = db[collection]
+        return collection
